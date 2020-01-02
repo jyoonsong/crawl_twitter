6f5f56385bd255bc54d443684f702f13fcee56ba
Jaeyoon Song <jyo3on@gmail.com>
2020-01-02 16:20:08 +0900

    collect data

--- main.py
+++ main.py
@@ -10,3 +10,26 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
+
+users = ["JoeBiden", "realDonaldTrump", "SenSanders", "VP"]
+
+for username in users:
+	user = api.get_user(username)
+
+	user_timeline = api.user_timeline(user.id)
+	tweets = []
+
+	if (username == "JoeBiden" || username == "SenSanders"):
+		ideology = "dem"
+	else:
+		ideology = "rep"
+
+
+	for tweet in user_timeline:
+		print("{")
+	    print("\ttext: " + tweet + ",")
+	    print("\tideology:" + ideology + ",")
+	    print("\tauthor: " + username + ",")
+	    print("\tisFallacy: false")
+	    print("},")
+
