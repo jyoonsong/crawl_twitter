49750208ab91390de90df5aac0e3257ca51e6893
Jaeyoon Song <jyo3on@gmail.com>
2020-01-02 16:19:50 +0900

    api

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
